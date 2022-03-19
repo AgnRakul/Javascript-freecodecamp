2bf055d90160a5e34489d608019d98e8b92a3786
RakulAgn <rakul0agn@gmail.com>
2022-03-19 19:52:54 +0530

    ES6 Remaining Done

--- ES6/create-strings-using-template-literals.js
+++ ES6/create-strings-using-template-literals.js
@@ -4,13 +4,13 @@ const result = {
   skipped: ["no-extra-semi", "no-dup-keys"]
 };
 function makeList(arr) {
-  // Only change code below this line
+  "use strict";
+  // change code below this line
   const failureItems = [];
   for (let i = 0; i < arr.length; i++) {
     failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
   }
-  // Only change code above this line
-
+  // change code above this line
   return failureItems;
 }
 
